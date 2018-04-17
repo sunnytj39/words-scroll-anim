4d55b9612be277fc414a7933d23abac85cc7f132
Kenta Kato <sunnytj39@gmail.com>
2018-04-17 17:46:07 +0900

    Fix main.js

--- main.js
+++ main.js
@@ -1,43 +1,51 @@
+// アニメーション間隔(s)
+const DURATION = 6;
+
+// アニメーション対象取得
 var anim_targets = document.getElementsByClassName("slide-target");
 var anim_wrappers = document.getElementsByClassName("slide-wrapper");
 
-console.log(anim_targets);
-
 [].forEach.call(anim_targets, function(target, i) {
-console.log(target, i);
 
+  // 要素の幅を取得
   var target_width = target.offsetWidth;
   var wrapper_width = anim_wrappers[i].offsetWidth;
-  var diff = target_width - wrapper_width;
-  
-  console.log(diff);
 
-  function resetAnim() {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        target.style.transition = '0s';
-        target.style.transform = 'translateX(0)';
-        resolve();
-      }, 6000);
-    })
-  }
-  
-  function startAnim() {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        target.style.transition = '5s linear';
-        target.style.transform = `translateX(-${diff}px)`;
-        resolve();
-      }, 1000);
-    })
-  }
-  
-  async function anim() {
-    while(true) {
-      const start = await startAnim();
-      const reset = await resetAnim();
+  // 親要素と文字列の差を計算
+  var diff = target_width - wrapper_width;
+
+  // 親要素からはみ出している場合スクロール適用
+  if (diff > 0) {
+    // 元の位置に戻す
+    function resetAnim() {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          target.style.transition = '0s';
+          target.style.transform = 'translateX(0)';
+          resolve();
+        }, DURATION * 1000);
+      })
     }
+    
+    // スクロール開始
+    function startAnim() {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          target.style.transition = `${DURATION}s linear`;
+          target.style.transform = `translateX(-${diff}px)`;
+          resolve();
+        }, 1000);
+      })
+    }
+
+    // 繰り返す
+    async function anim() {
+      while(true) {
+        const start = await startAnim();
+        const reset = await resetAnim();
+      }
+    }
+
+    anim();
   }
-  
-  anim();
 })
